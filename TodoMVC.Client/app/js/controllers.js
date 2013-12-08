2645c8bc303318c18d9865b4bba2b0ad7ce834a6
AdriVanHoudt <adri.van.houdt@gmail.com>
2013-12-08 18:49:22 +0100

    added create page; only post needed

--- TodoMVC.Client/app/js/controllers.js
+++ TodoMVC.Client/app/js/controllers.js
@@ -6,3 +6,12 @@ var controllers = angular.module('app.controllers', []);
 controllers.controller('TodoList_ListCtrl', ['$scope', 'TodoList', function ($scope, TodoList) {
     $scope.todoLists = TodoList.query();
 }]);
+
+controllers.controller('TodoList_CreateCtrl', ['$scope', function ($scope) {
+    $scope.toDoListName;
+    $scope.text = "Geef hier de naam in van de nieuwe todolist";
+    $scope.setTodoList = function(name) {
+        //TODO post to server
+        $scope.toDoListName = name;   
+    }
+}]);
